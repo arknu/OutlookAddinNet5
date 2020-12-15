2da432e892ad20542ab2d8ce8001d0def6c684e6
Asbjørn Riis-Knudsen <ar@jf-data.com>
2020-12-15 10:40:33 +0100

    Add project files.

---
+++ OutlookAddinNet5/AddinRibobn.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Office = Microsoft.Office.Core;
+
+namespace OutlookAddinNet5
+{
+    public partial class ThisAddin : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI _ribbon;
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonId)
+        {
+            return Properties.Resources.Ribbon;
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+
+        public bool IsRibbonCommandEnabled(Office.IRibbonControl ctrl)
+        {
+            return IsCommandEnabled(ctrl.Id);
+        }
+
+        public bool IsRibbonCommandChecked(Office.IRibbonControl ctrl)
+        {
+            return IsCommandChecked(ctrl.Id);
+        }
+
+        public void OnRibbonButtonCheckClick(Office.IRibbonControl control, bool pressed)
+        {
+            OnCommand(control.Id);
+        }
+
+        public void OnRibbonButtonClick(Office.IRibbonControl control)
+        {
+            OnCommand(control.Id);
+        }
+
+        public string OnGetRibbonLabel(Office.IRibbonControl control)
+        {
+            return GetCommandLabel(control.Id);
+        }
+
+        public void OnRibbonLoad(Office.IRibbonUI ribbonUI)
+        {
+            _ribbon = ribbonUI;
+        }
+
+        public Bitmap GetRibbonImage(Office.IRibbonControl control)
+        {
+            return GetCommandBitmap(control.Id);
+        }
+
+        #endregion
+
+        public void UpdateRibbon()
+        {
+            _ribbon.Invalidate();
+        }
+    }
+}
---
+++ OutlookAddinNet5/ThisAddin.cs
@@ -0,0 +1,136 @@
+﻿using AddInDesignerObjects;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookAddinNet5
+{
+    [ComVisible(true)]
+    [Guid("011150D8-70D0-42C2-B00A-3A1290399760")]
+    [ProgId("OutlookAddinNet5")]
+    public partial class  ThisAddin : _IDTExtensibility2
+    {
+        public Outlook.Application Application { get; set; }
+
+        /// <summary>
+        /// A simple command
+        /// </summary>
+        public void Command1()
+        {
+            MessageBox.Show(
+                "Hello from command 1!",
+                "VisioComAddinNet5");
+        }
+
+        /// <summary>
+        /// A command to demonstrate conditionally enabling/disabling.
+        /// The command gets enabled only when a shape is selected
+        /// </summary>
+        public void Command2()
+        {
+           
+            MessageBox.Show(
+                string.Format("Hello from (conditional) command 2!"));
+        }
+
+        /// <summary>
+        /// Callback called by the UI manager when user clicks a button
+        /// Should do something meaningful when corresponding action is called.
+        /// </summary>
+        public void OnCommand(string commandId)
+        {
+            switch (commandId)
+            {
+                case "Command1":
+                    Command1();
+                    return;
+
+                case "Command2":
+                    Command2();
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Callback called by UI manager.
+        /// Should return if corresponding command should be enabled in the user interface.
+        /// By default, all commands are enabled.
+        /// </summary>
+        public bool IsCommandEnabled(string commandId)
+        {
+            switch (commandId)
+            {
+                case "Command1":    // make command1 always enabled
+                    return true;
+
+                case "Command2":    // make command2 enabled only if a drawing is opened
+                    return true;
+                default:
+                    return true;
+            }
+        }
+
+        /// <summary>
+        /// Callback called by UI manager.
+        /// Should return if corresponding command (button) is pressed or not (makes sense for toggle buttons)
+        /// </summary>
+        public bool IsCommandChecked(string command)
+        {
+            return false;
+        }
+        /// <summary>
+        /// Callback called by UI manager.
+        /// Returns a label associated with given command.
+        /// We assume for simplicity taht command labels are named simply named as [commandId]_Label (see resources)
+        /// </summary>
+        public string GetCommandLabel(string command)
+        {
+            return Properties.Resources.ResourceManager.GetString(command + "_Label");
+        }
+
+        /// <summary>
+        /// Returns a bitmap associated with given command.
+        /// We assume for simplicity that bitmap ids are named after command id.
+        /// </summary>
+        public Bitmap GetCommandBitmap(string id)
+        {
+            var obj = Properties.Resources.ResourceManager.GetObject(id);
+            return obj as Bitmap;
+        }
+
+        internal void UpdateUI()
+        {
+            UpdateRibbon();
+        }
+
+
+
+        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            this.Application = (Outlook.Application)Application;
+        }
+
+        public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+            
+        }
+
+        public void OnAddInsUpdate(ref Array custom)
+        {
+            
+        }
+        
+        public void OnStartupComplete(ref Array custom)
+        {
+            
+        }
+
+        public void OnBeginShutdown(ref Array custom)
+        {
+            
+        }
+    }
+}
