45ee04d10c59bb71cba1b1783fdbec954f0c7014
Asbjørn Riis-Knudsen <ar@jf-data.com>
2021-03-14 13:18:54 +0100

    Add IComDefaultInterface

--- OutlookAddinNet5/TestTaskPane.cs
+++ OutlookAddinNet5/TestTaskPane.cs
@@ -11,11 +11,13 @@ using System.Windows.Forms;
 
 namespace OutlookAddinNet5
 {
+    public interface ITestTaskPane { }
+
     //[ProgId("OutlookAddinNet5.TestTaskPane")]
     [Guid("EBC1A7F3-DD4B-4EBE-9DDE-4A7640E4434B")]
-
     [ComVisible(true)]
-    public partial class TestTaskPane : UserControl
+    [ComDefaultInterface(typeof(ITestTaskPane))] // being explicit is required, reflection based IDispatch does not work
+    public partial class TestTaskPane : UserControl, ITestTaskPane
     {
         public TestTaskPane()
         {
